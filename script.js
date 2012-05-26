a5b52385e14cdfb5c7061c46142977bd230f84d3
Albert Hwang <hwang.al@gmail.com>
2012-05-26 11:58:56 -0400

    template for bookmarklet

---
+++ script.js
@@ -0,0 +1,28 @@
+(function(){
+
+  // the minimum version of jQuery we want
+  var v = "1.3.2";
+
+  // check prior inclusion and version
+  if (window.jQuery === undefined || window.jQuery.fn.jquery < v) {
+    var done = false;
+    var script = document.createElement("script");
+    script.src = "http://ajax.googleapis.com/ajax/libs/jquery/" + v + "/jquery.min.js";
+    script.onload = script.onreadystatechange = function(){
+      if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
+        done = true;
+        initMyBookmarklet();
+      }
+    };
+    document.getElementsByTagName("head")[0].appendChild(script);
+  } else {
+    initMyBookmarklet();
+  }
+
+  function initMyBookmarklet() {
+    (window.myBookmarklet = function() {
+      console.log("hi there");
+    })();
+  }
+
+})();
