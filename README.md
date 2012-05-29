29652010a600b30a4c7ca3943e6d40e4dbf18ed9
Albert Hwang <hwang.al@gmail.com>
2012-05-29 12:23:21 -0400

    moved it over to a control panel

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-bleepify-site
-=============
+bleepify
+========
+
+
+After installing this bookmarklet, you'll be able to listen to any website you want to!
+
+When turned on, *the bookmarklet will sequentially pass through each element on the page, ringing out a note.  The pitch of the note is determined by how deeply nested the element sits in the page.
+
+Interesting rhythms begin to emerge organically.  Each website has it's own set of musical tropes based on the arrangements modularized code that they use.  <-- thank my artsy-fartsy education for that artsy-fartsy paragraph : )
+
+Anyhow, deets and examples available at:
+
+bleepify.albert-hwang.com
--- script.js
+++ script.js
@@ -12,15 +12,21 @@
     script.onload = script.onreadystatechange = function(){
       if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
         done = true;
-        initMyBookmarklet();
+        initMyBleepify();
       }
     };
     document.getElementsByTagName("head")[0].appendChild(script);
   } else {
-    initMyBookmarklet();
+    initMyBleepify();
   }
+  var style = document.createElement("link");
+  style.type = "text/css";
+  style.rel = "stylesheet";
+  style.href = "http://bleepify.albert-hwang.com/style.css?r=" + Math.floor(Math.random()*80000);
+  //style.href = "/style.css?r=" + Math.floor(Math.random()*80000);
+  document.getElementsByTagName("head")[0].appendChild(style);
 
-  function initMyBookmarklet() {
+  function initMyBleepify() {
 
     (window.myBookmarklet = function() {
       
@@ -39,17 +45,65 @@
 
 
         // request.open( 'GET', '26777__junggle__btn402.mp3', true );
-        request.open( 'GET', 'http://staging.albert-hwang.com/26777__junggle__btn402.mp3', true );
+        request.open( 'GET', 'http://bleepify.albert-hwang.com/26777__junggle__btn402.mp3', true );
         request.responseType = "arraybuffer";
         request.send();
 
-        var major = [1, 9/8, 5/4, 4/3, 3/2, 5/3, 15/8];
-        var minor = [1, 9/8, 6/5, 4/3, 3/2, 8/5, 9/5]; // of course, for now, w/out context, this is just a major scale of another note...
-        var blues = [1, 6/5, 4/3, 45/32, 3/2, 9/5];
-        var chromatic = [1, 16/15, 9/8, 6/5, 5/4, 4/3, 45/32, 3/2, 8/5, 5/3, 9/5, 15/8];
-        var arpeggio = [1, 5/4, 3/2];
-        var arpeggio7 = [1, 5/4, 3/2, 15/8];
-        
+        var ctrl ='';
+        ctrl=ctrl+ '<div id="bleepifyControllerWrap">';
+        ctrl=ctrl+ '  <div id="bleepifyController">';
+        ctrl=ctrl+ '    <div class="bleepifyH1">Bleepify It!</div>';
+        ctrl=ctrl+ '    <div>';
+        ctrl=ctrl+ '      <div class="bleepifyH2">Time Between Bleeps</div>';
+        ctrl=ctrl+ '      <input id="bleepifySecondsBtwn" type="text" name="bleepifySecondsBtwn" value=".075" />';
+        ctrl=ctrl+ '      <label style="font-size: 16px; color: #00ff00;">Seconds</label>';
+        ctrl=ctrl+ '    </div>';
+        ctrl=ctrl+ '    <div>';
+        ctrl=ctrl+ '      <div class="bleepifyH2">Scale</div>';
+        ctrl=ctrl+ '      <a href="#" class="scale-selector active" id="bleepify-major">Major</a>';
+        ctrl=ctrl+ '      <a href="#" class="scale-selector" id="bleepify-chromatic">Chromatic</a>';
+        ctrl=ctrl+ '      <a href="#" class="scale-selector" id="bleepify-blues">Blues</a>';
+        ctrl=ctrl+ '      <a href="#" class="scale-selector" id="bleepify-arpeggio">Arpeggio</a>';
+        ctrl=ctrl+ '      <a href="#" class="scale-selector" id="bleepify-arpeggio7">Arpeggio7</a>';
+        ctrl=ctrl+ '    </div>';
+        ctrl=ctrl+ '    <a href="#" id="runBleepify">Run</a>';
+        ctrl=ctrl+ '    <div style="clear: both;"></div>';
+        ctrl=ctrl+ '  </div>';
+        ctrl=ctrl+ '  <a href="#" id="bleepifyOpenClose" title="hide panel" alt="hide panel">hide / show Bleepify</a>';
+        ctrl=ctrl+ '</div>';
+        jQuery(ctrl).appendTo(jQuery("body"));
+        jQuery("#bleepifyOpenClose").click(function() {
+          jQuery("#bleepifyController").slideToggle();
+          return false;
+        });
+        jQuery('.scale-selector').click(function() {
+          jQuery('.scale-selector.active').removeClass('active');
+          jQuery(this).addClass("active");
+          return false;
+        });
+        var letBleepifyRun = true;
+        jQuery('#runBleepify').click(function() {
+          if (!jQuery(this).hasClass('bleepify-running')) {
+            jQuery(this).addClass("bleepify-running").html("Stop");
+            letBleepifyRun = true;
+            var scaleToRun = jQuery(".scale-selector.active").attr("id").replace("bleepify-", "");
+            console.log(scaleToRun);
+            var delayTime = jQuery("#bleepifySecondsBtwn").attr("value") * 1000.0;
+            bleepify(jQuery("body"), scales[scaleToRun], delayTime);
+          } else {
+            cleanupBleepify();
+          }
+        });
+
+        var scales = {
+          "major": [1, 9/8, 5/4, 4/3, 3/2, 5/3, 15/8],
+          "minor": [1, 9/8, 6/5, 4/3, 3/2, 8/5, 9/5],
+          "blues": [1, 6/5, 4/3, 45/32, 3/2, 9/5],
+          "chromatic": [1, 16/15, 9/8, 6/5, 5/4, 4/3, 45/32, 3/2, 8/5, 5/3, 9/5, 15/8],
+          "arpeggio": [1, 5/4, 3/2],
+          "arpeggio7": [1, 5/4, 3/2, 15/8]
+        };
+
         function playSound(depth, scale){
           var newSource = context.createBufferSource();
           newSource.buffer = source.buffer;
@@ -63,17 +117,58 @@
           newSource.noteOn( 0 );
         };
 
-      jQuery(':visible').each(function(index){
-        var $this = jQuery(this);
-        var oldBorder = $this.css("border");
-        setTimeout(function () {
-          $this.css("border", "1px solid red");
-          playSound($this.parents().length, major);
-        }, (index + 3) * 400);
-        setTimeout(function() {
-          $this.css("border", oldBorder);
-        }, (index + 4) * 400);
-      });
+      function bleepify($el, scale, delayTime) {
+        if (!$el.hasClass('alreadyBleeped')) {
+          $el.addClass('alreadyBleeped');
+          setTimeout(function () {
+            var formerlyBeeping = jQuery(".bleeping");
+            if (formerlyBeeping.length > 0) {
+              formerlyBeeping.css("border", formerlyBeeping.data("oldBorder")).removeClass("bleeping");
+            }
+            $el.addClass("bleeping").data("oldBorder", $el.css("border")).css("border", "1px solid red");
+            playSound($el.parents().length, scale);
+            bleepifyNextItem($el, scale, delayTime);
+          }, delayTime);
+        } else {
+          bleepifyNextItem($el, scale, delayTime);
+        }
+      }
+      
+      function bleepifyNextItem($el, scale, delayTime) {
+        var nextItem;
+        if ($el.children(':visible').length > 0 && (! $el.hasClass("ancestorsExplored"))) {
+          $el.addClass("ancestorsExplored");
+          nextItem = $el.children(":visible").eq(0);
+        } else if ($el.next(":visible").length > 0) {
+          nextItem = $el.next(":visible");
+        } else if ($el.parent(":visible").length > 0){
+          nextItem = $el.parent(":visible");
+        }
+        if ($el.parent(":visible").length == 0) {
+          cleanupBleepify();
+        } else {
+          if (letBleepifyRun) {
+            bleepify(nextItem, scale, delayTime);
+          } else {
+            cleanupBleepify();
+          }
+        }
+      }
+      
+      function cleanupBleepify() {
+        jQuery(".ancestorsExplored").removeClass("ancestorsExplored");
+        jQuery(".alreadyBleeped").removeClass("alreadyBleeped");
+        var formerlyBeeping = jQuery(".bleeping");
+        if (formerlyBeeping.length > 0) {
+          formerlyBeeping.css("border", formerlyBeeping.data("oldBorder")).removeClass("bleeping");
+        }
+        jQuery("#runBleepify").removeClass("bleepify-running").html("Run");
+        letBleepifyRun = false;
+      }
+
+      // setTimeout(function () {
+      //   bleepify(jQuery('body'), major, 100);
+      // }, 500);
     })();
   }
 
